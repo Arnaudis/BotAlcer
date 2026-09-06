2c4fd388076acee6cd84b9505b81b0b5a9bfb977
Arnaudis Suárez Sebastián <47118718+Arnaudis@users.noreply.github.com>
2026-09-06 20:02:38 +0100

    20260906... Control de tiempos

--- BotAlcer.py
+++ BotAlcer.py
@@ -17,6 +17,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
 from langchain_ollama import ChatOllama
+import time
 
 # La libería PyPDFLoader genera un DeprecationWarning y queremos que no aparezca.
 warnings.filterwarnings("ignore", category=DeprecationWarning)
