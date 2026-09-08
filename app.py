fb2ad0561ce9c8c2094568dff0d40a6e886706d3
Arnaudis Suárez Sebastián <47118718+Arnaudis@users.noreply.github.com>
2026-09-08 13:54:31 +0100

    20260908... CSS

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ st.markdown(
 
     /* Subimos el logo y bajamos el título */
     [data-testid="stHorizontalBlock"] {
-        margin-top: -7em !important;    /* Desplaza la imagen hacia arriba para absorber el vacío */
+        margin-top: -6em !important;    /* Desplaza la imagen hacia arriba para absorber el vacío */
         margin-bottom: 1rem !important; /* Contrae el espacio vacío de la parte inferior de la imagen */
     }
 
@@ -92,17 +92,16 @@ st.markdown(
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 LOGO_PATH = os.path.join(BASE_DIR, "pictures", "logoAlcer.png")
 
-col1, col2, col3 = st.columns([1, 6, 1])
+col1, col2, col3 = st.columns([1, 10, 1])
 with col2:
     # Al estar dentro de col2, st.image centrará el logo automáticamente en el medio de la web
     if os.path.exists(LOGO_PATH):
-        st.image(LOGO_PATH, width=400)
+        st.image(LOGO_PATH, width=380)
     else:
         st.error(f"No se encontró el logo en: {LOGO_PATH}")
 
 #st.title("🏥 BotAlcer")
-#st.subheader("Asistente experto en Enfermedad Renal Crónica")
-st.title("Asistente en Enfermedad Renal Crónica (ERC) de ALCER Las Palmas")
+st.subheader("Asistente en Enfermedad Renal Crónica (ERC) de ALCER Las Palmas")
 
 
 
